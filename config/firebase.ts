cfc93294072e9298f90228a73670518099e84a73
nguyennp305 <20021406@vnu.edu.vn>
2023-02-06 23:58:46 +0700

    getting start

---
+++ config/firebase.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa4u06f6UzvIrnSgcAy7FlFTpQrUoABEU",
+  authDomain: "whatsapp-clone-49dbb.firebaseapp.com",
+  projectId: "whatsapp-clone-49dbb",
+  storageBucket: "whatsapp-clone-49dbb.appspot.com",
+  messagingSenderId: "323687596563",
+  appId: "1:323687596563:web:9b0112c67f42adc8be35e6"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp(): initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider()
+
+export {db, auth, provider}
+
+
---
+++ pages/login.tsx
@@ -0,0 +1,49 @@
+import Button from '@mui/material/Button'
+import Head from 'next/head'
+import React from 'react'
+import styled from 'styled-components'
+import Image from 'next/image'
+import WhatsAppLogo from '.././assets/whatsapplogo.png'
+
+
+
+const StyledContainer = styled.div`
+height: 100vh;
+display: grid;
+place-items: center;
+background-color: whitesmoke;
+`
+
+const StyledLoginContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 100px;
+background-color: white;
+bordere-radius: 5px;
+box-shadow: 0px 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0/ 0.1);
+`
+
+const StyledImageWrapper = styled.div`
+margin-bottom: 50px;
+`
+const Login = () => {
+  return (
+    <StyledContainer>
+        <Head>
+            <title>Login</title>
+        </Head>
+
+        <StyledLoginContainer>
+            <StyledImageWrapper>
+                <Image src={WhatsAppLogo} width='200' height='200' alt='WhatsApp Logo' />
+            </StyledImageWrapper>
+            <Button variant='outlined' onClick={() => {
+                console.log('Sign in ')
+            }}>Sign in With google</Button>
+        </StyledLoginContainer>
+    </StyledContainer>
+  )
+}
+
+export default Login
